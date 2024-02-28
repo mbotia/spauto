60481dd425e5757ed4c9ac34d47d5b46b7e1e03c
mbotia <mbotia@gmail.com>
2024-02-28 01:58:14 +0000

    netconf ok

--- netconf/get_device_info.py
+++ netconf/get_device_info.py
@@ -28,22 +28,59 @@ interface_filter = ['''
                     </filter>
                     ''']
 
+static_filter = ['''
+                <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+                        <ip>
+                        <route/>
+                        </ip>
+                    </native>
+                </filter>
+                ''',' '
+                 ]
+
+ospf_filter = ['''
+                <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+                    <router>
+                    <router-ospf xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-ospf"/>
+                    </router>
+                </native>
+                </filter>
+                ''',' '
+                 ]
+
+bgp_filter = ['''
+                <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+                    <router>
+                        <bgp xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-bgp"/>
+                    </router>
+                </native>
+                </filter>
+                ''',' '
+                 ]
+
+
 interface_tag = ["name", "interface-name"]
 hostname_tag = ["hostname", "host-name"]
+static_tag = ["ip-route-interface-forwarding-list",""]
 
 devices = [{"HOST":"192.168.200.81", "USER":"cisco", "PASSWORD":"cisco", "PLATFORM" : "iosxe" },
            {"HOST":"192.168.200.82", "USER":"cisco", "PASSWORD":"cisco", "PLATFORM" : "iosxr"}]
 
 
 for device in devices:
-    with manager.connect(host=device["HOST"], port=830,  device_params={"name":device["PLATFORM"]},
-                            username=device["USER"], password=device["PASSWORD"], hostkey_verify = False ) as m:
+    with manager.connect (host = device["HOST"], username=device["USER"], password = device["PASSWORD"], 
+                          device_params = { "name": device["PLATFORM"]},
+                          hostkey_verify=False, look_for_keys=False) as m:
         try:    
             if device["PLATFORM"] == "iosxe":
                 index = 0
             else:
                 index = 1
 
+            # GET HOSTNAME INFO
             result = m.get_config("running", hostname_filter[index])          
             # Pass the XML result as a string to the parseString function
             xml_doc = xml.dom.minidom.parseString(result.xml)
@@ -51,18 +88,38 @@ for device in devices:
             hostname = xml_doc.getElementsByTagName(hostname_tag[index])
             print(hostname[0].firstChild.nodeValue)
 
+            # GET INTERFACE INFO
+            print("############# INTERFACES #############")
             result = m.get_config('running', interface_filter[index])
             xml_doc = xml.dom.minidom.parseString(result.xml)
             #print(xml_doc.toprettyxml())
             interfaces = []
             interface_name = xml_doc.getElementsByTagName(interface_tag[index])
             number_int = len(interface_name)
-            index = 0
-            while index < number_int:
-                interfaces.append(interface_name[index].firstChild.nodeValue)
-                index += 1
-
+            index_ = 0
+            while index_ < number_int:
+                interfaces.append(interface_name[index_].firstChild.nodeValue)
+                index_ += 1
             print(interfaces)
+
+            #GET STATIC ROUTES INFO
+            print("############# STATIC ROUTES INFO #############")
+            result = m.get_config('running', static_filter[index])
+            xml_doc = xml.dom.minidom.parseString(result.xml)
+            static_info = xml_doc.getElementsByTagName(static_tag[index])
+            print(xml_doc.toprettyxml())
+
+            #GET OSPF INFO
+            print("############# OSPF INFO #############")
+            result = m.get_config('running', ospf_filter[index])
+            xml_doc = xml.dom.minidom.parseString(result.xml)
+            print(xml_doc.toprettyxml())
+
+            #GET BGP INFO
+            print("############# BGP INFO #############")
+            result = m.get_config('running', bgp_filter[index])
+            xml_doc = xml.dom.minidom.parseString(result.xml)
+            print(xml_doc.toprettyxml())
 
         except:
             print("N/A")
